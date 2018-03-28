7ad9537903a64442d37e3dd5ce22695b517999f3
Neeraj Jaiswal <neerajj85@gmail.com>
2018-03-28 16:56:00 +0530

    first commit

---
+++ src/com/step/bootcamp/Rectangle.java
@@ -0,0 +1,23 @@
+package com.step.bootcamp;
+
+
+/*
+Understands enclosed shape with 4 corners at right angles
+ */
+public class Rectangle {
+  private final double length;
+  private final double breadth;
+
+  public Rectangle(double length, double breadth) {
+    this.length = length;
+    this.breadth = breadth;
+  }
+
+  public double area() {
+    return length * breadth;
+  }
+
+  public double perimeter() {
+    return 2*(length+breadth);
+  }
+}
---
+++ test/com/step/bootcamp/SquareTest.java
@@ -0,0 +1,43 @@
+package com.step.bootcamp;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/*
+Ensures the correctness of area, perimeter of square
+ */
+public class SquareTest {
+
+  private Square square;
+
+  @Before
+  public void setUp() {
+    square = new Square(8);
+  }
+
+  @Test
+  public void shouldReturnAreaWhenSideIs5() {
+    Square square = new Square(5);
+    assertThat(square.area(),is(25.0));
+  }
+
+  @Test
+  public void shouldReturnArea() {
+    assertThat(square.area(), is(64.0));
+  }
+
+  @Test
+  public void shouldReturnPerimeterWhenSideIs5() {
+    Square square = new Square(5);
+    assertThat(square.perimeter(),is(20.0));
+  }
+
+  @Test
+  public void shouldReturnPerimeter() {
+    assertThat(square.perimeter(), is(32.0));
+  }
+}
